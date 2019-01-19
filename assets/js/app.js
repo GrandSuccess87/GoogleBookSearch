307ea0df36c0059315e56eca9764051d41b174fb
Quintessa <anderson.quintessa@gmail.com>
2019-01-18 18:21:55 -0600

    pushing code for initial app set up

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+function renderBooks(argument) {
+	console.log("Render Books Function Successfully Loaded");
+}
+
+document.getElementById('searchButton').addEventListener('click', renderBooks, false);
+
+
+ 
