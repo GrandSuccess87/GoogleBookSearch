08f74b3fa6cd93b68c056cebefb38f35c5968b22
Quintessa <anderson.quintessa@gmail.com>
2019-01-19 11:29:50 -0600

    adding bootstrap and css/jquery cdn

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,17 @@
+// connect js and html
 function renderBooks(argument) {
 	console.log("Render Books Function Successfully Loaded");
 }
 
 document.getElementById('searchButton').addEventListener('click', renderBooks, false);
 
+// example of embedded viewer 
+google.books.load();
 
+      function initialize() {
+        var viewer = new google.books.DefaultViewer(document.getElementById('viewerCanvas'));
+        viewer.load('ISBN:0738531367');
+      }
+
+      google.books.setOnLoadCallback(initialize);
  
