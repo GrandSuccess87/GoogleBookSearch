a46110e55b72e2ff9e91b5e8b60648ef3a7d138c
Quintessa <anderson.quintessa@gmail.com>
2019-01-19 13:00:09 -0600

    pushing code to perform a search, working on ajax function error

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,33 @@
 // connect js and html
 function renderBooks(argument) {
 	console.log("Render Books Function Successfully Loaded");
+	const search = document.querySelector('#search').value;
+	// const search = $('#search').val().trim();
+	console.log(search);
+
+	$.ajax({
+		url: "https://www.googleapis.com/books/v1/volumes?q=" + search,
+		// + intitle + inauthor + inpublisher + key=AIzaSyDyNOO9LVN_GjuouA8pqHHRSXtBB_RZO8Y
+		type: 'GET',
+		data: search,
+		dataType: "json",
+		success: function(data) {
+		 console.log(data)
+		}
+	});
 }
 
 document.getElementById('searchButton').addEventListener('click', renderBooks, false);
 
+
+
 // example of embedded viewer 
-google.books.load();
+// google.books.load();
 
-      function initialize() {
-        var viewer = new google.books.DefaultViewer(document.getElementById('viewerCanvas'));
-        viewer.load('ISBN:0738531367');
-      }
+//       function initialize() {
+//         var viewer = new google.books.DefaultViewer(document.getElementById('viewerCanvas'));
+//         viewer.load('ISBN:0738531367');
+//       }
 
-      google.books.setOnLoadCallback(initialize);
+//       google.books.setOnLoadCallback(initialize);
  
