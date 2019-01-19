ca9261417cf6c32292ca226fc76ce043ea8f0474
Quintessa <anderson.quintessa@gmail.com>
2019-01-19 14:45:30 -0600

    pushing code for succesful search request

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,46 @@
 // connect js and html
 function renderBooks(argument) {
 	console.log("Render Books Function Successfully Loaded");
+	// Grab the input from the user
 	const search = document.querySelector('#search').value;
 	// const search = $('#search').val().trim();
 	console.log(search);
 
+	// send an HTTP GET request to the API
 	$.ajax({
 		url: "https://www.googleapis.com/books/v1/volumes?q=" + search,
 		// + intitle + inauthor + inpublisher + key=AIzaSyDyNOO9LVN_GjuouA8pqHHRSXtBB_RZO8Y
 		type: 'GET',
 		data: search,
-		dataType: "json",
-		success: function(data) {
-		 console.log(data)
-		}
+		dataType: "json"
+	}).then(function(callbackData) {
+		
+		// log the callback data
+		 console.log(callbackData);
+
+		 const data = callbackData.items;
+
+
+		 // loop through the books that match the query
+		 for(var i = 0; i < data.length; i++) {
+
+
+		 	console.log(data[i].volumeInfo.title);
+		 	console.log(data[i].volumeInfo.authors);
+		 	console.log(data[i].volumeInfo.publisher);
+			console.log(data[i].volumeInfo.imageLinks.thumbnail);
+			console.log(data[i].volumeInfo.imageLinks.smallThumbnail);
+			console.log(data[i].volumeInfo.previewLink);
+
+			const book = data[i].volumeInfo;
+			const title = book.title;
+			const author = book.authors;
+			const image = book.imageLinks.thumbnail;
+			const smallThumbnail = book.imageLinks.smallThumbnail;
+			const preview = book.previewLink;
+		 }
+
+	
 	});
 }
 
