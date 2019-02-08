9a16c7708697007f4d257cb335f72a080db787ec
Quintessa <anderson.quintessa@gmail.com>
2019-02-07 19:17:52 -0600

    pushing code for improving internal quality - fixed the formatting and indentations of app.js, fixed viewCanvas typo

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,15 +8,15 @@ function bookQuery(event) {
 	if(search == '' || search == null) {
 		alert('Your search field is empty.  Please enter the book or author you would like to search.');
 	} else {
-	// Sends an HTTP GET request to the API
-	$.ajax({
-		url: 'https://www.googleapis.com/books/v1/volumes?q=' + search + '&startIndex=0' + '&maxResults=30',
-		type: 'GET',
-		data: search,
-		dataType: 'json'
-	}).then(function(callbackData) {
-		renderBook(callbackData);
-	});
+		// Sends an HTTP GET request to the API
+		$.ajax({
+			url: 'https://www.googleapis.com/books/v1/volumes?q=' + search + '&startIndex=0' + '&maxResults=30',
+			type: 'GET',
+			data: search,
+			dataType: 'json'
+		}).then(function(callbackData) {
+			renderBook(callbackData);
+		});
 	}
 };
 
@@ -27,57 +27,46 @@ function getSearchInput(searchInput) {
 
 function setEnterButton () {
 	document.querySelector('#search').addEventListener('keyup', function(event) {
-	  event.preventDefault();
-	  if (event.keyCode === 13) {
+		event.preventDefault();
+		if (event.keyCode === 13) {
 		document.getElementById('searchButton').click();
-	  }
+		}
 	});
 };
 
 // Loops through the callbackData
 function renderBook(callbackData) {
 	const data = callbackData.items;
-	const viewerCanvas = document.getElementById('viewerCanvas')
+	const viewerCanvas = document.getElementById('viewerCanvas');
 	$('#viewerCanvas').empty();
-
 	// Loops through the book data
 	data.forEach(function(bookData) {
-	// Declare variables for the book data
-	const book = bookData.volumeInfo;
-
-	// Creates a new div for each book in the array
-	const newBookDiv = document.createElement('div');
-	newBookDiv.setAttribute('class', 'book')
-
-	// assign the title, author, publisher, image, and preview link then append to the new div
-
-	// title
-	setBookTitleAttributes(book, localStorage);
-
-	// author
-	setBookAuthorAttributes(book, localStorage);
-
-	// publisher
-	setBookPublisherAttributes (book, localStorage);
-
-	// book image
-	setBookImageAttributes(book, localStorage);
-
-	// preview
-	setBookPreview (book, localStorage);
-
-	// Appends all data to the newBookDiv
-	newBookDiv.appendChild(link_section);
-	newBookDiv.appendChild(title_section);
-	newBookDiv.appendChild(auth_section);
-	newBookDiv.appendChild(pub_section);
-	newBookDiv.appendChild(bookImageLink);
-
-	// Appends new book div to the viewCanvas div
-	viewerCanvas.appendChild(newBookDiv);
-
-	//Empties search div
-	$('#search').val('');
+		// Declare variables for the book data
+		const book = bookData.volumeInfo;
+		// Creates a new div for each book in the array
+		const newBookDiv = document.createElement('div');
+		newBookDiv.setAttribute('class', 'book');
+		// assign the title, author, publisher, image, and preview link then append to the new div
+		// title
+		setBookTitleAttributes(book, localStorage);
+		// author
+		setBookAuthorAttributes(book, localStorage);
+		// publisher
+		setBookPublisherAttributes (book, localStorage);
+		// book image
+		setBookImageAttributes(book, localStorage);
+		// preview
+		setBookPreview (book, localStorage);
+		// Appends all data to the newBookDiv
+		newBookDiv.appendChild(link_section);
+		newBookDiv.appendChild(title_section);
+		newBookDiv.appendChild(auth_section);
+		newBookDiv.appendChild(pub_section);
+		newBookDiv.appendChild(bookImageLink);
+		// Appends new book div to the viewerCanvas div
+		viewerCanvas.appendChild(newBookDiv);
+		//Empties search div
+		$('#search').val('');
 	});
 };
 
